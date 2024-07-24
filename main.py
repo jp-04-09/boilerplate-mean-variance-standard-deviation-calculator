47ab65bc3748b7a71d9d4f1df7ef7a8de12a5e3c
juan pazos <jppazos90@gmail.com>
2024-07-24 05:54:54 +0000

    checking length and type

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 import mean_var_std
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+try:
+    print(mean_var_std.calculate([0,1,2,3,4,5,6,7]))
+    print("All elements in the 9 elements of the array are numbers.")
+except ValueError as e:
+    print(f"Array check failed: {e}")
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,17 @@ import numpy as np
 
 def calculate(list):
 
-    b = 1
+    my_array = np.array(list)
+    # printing the array
+    print("The array is : " + str(my_array))
+    # Checking lenght and the type of the elements of the array
+    list_size = my_array.size
+    list_type = my_array.dtype
+    if list_size == 9 and np.issubdtype(my_array.dtype, np.number):
+        print("the array lenght is: " + str(list_size))
+        print("All elements of the array are numbers")
+    else:
+        raise ValueError("List must contain nine numbers.")
 
 
     return calculations
